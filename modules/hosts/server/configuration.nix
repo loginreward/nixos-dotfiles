a55f4a28f90861ffbe4ea65ef1609734d49c9eb1
loginreward <nolan.lessard.music@gmail.com>
2026-05-28 12:52:29 -0400

    feat(server): disabled system sleeping

--- modules/hosts/server/configuration.nix
+++ modules/hosts/server/configuration.nix
@@ -13,6 +13,11 @@
 
     virtualisation.libvirtd.enable = true;
 
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
     programs.nix-ld.enable = true;
     programs.nix-ld.libraries = with pkgs; [
         wayland
